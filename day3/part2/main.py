c0cb2745bd79da09d94ad692f647872ecdb4b9c7
Viktor Lindgren <viktor.lindgren@elasticmove.com>
2022-12-04 12:54:53 +0100

    Removed debugging

--- day3/part2/main.py
+++ day3/part2/main.py
@@ -5,11 +5,9 @@ sum_prio = 0
 with open("../input.txt", "r") as file:
 
     group = list()
-    count = 0
+
     for line in file:
         line = line.strip()
-        count += 1
-
         group.append(Rucksack(line))
         
         # Every 3rd rucksack, check overlap and save prio
