d299e7140d36f4214ca258d98f1a7900baa0d20e
Viktor Lindgren <viktor.lindgren@elasticmove.com>
2022-12-04 11:17:42 +0100

    Day 2 part 1 done

--- day2/part1/main.py
+++ day2/part1/main.py
@@ -1,6 +1,6 @@
 from rps import RockPaperScissors
 
-with open("input.txt", "r") as file:
+with open("../input.txt", "r") as file:
     
     tournament = RockPaperScissors()
 
--- day2/part1/rps.py
+++ day2/part1/rps.py
@@ -2,29 +2,33 @@
 class RockPaperScissors:
 
     # Order is rock, paper, scissors
-    throws = [ "A", "B", "C", "X", "Y", "Z"]
+    throws_opponent = [ "A", "B", "C" ]
+    throws_you = [ "X", "Y", "Z"]
 
     points = {
         "win": 6,
         "draw": 3,
         "loss": 0,
-        "X": 1,
-        "Y": 2,
-        "Z": 3
+        "X": 1, # Rock
+        "Y": 2, # Paper
+        "Z": 3 # Scissors
     }
 
     def __init__(self) -> None:
         self.total_score = 0
 
     def play(self, opponent: str, you: str) -> int:
+        # Convert throws to an int, R = 0, P = 1, S = 2
+        throw_you = self.throws_you.index(you)
+        throw_opp = self.throws_opponent.index(opponent)
 
         # Add score for your chosen throw
         round_score = self.points[you]
 
         # Add score for outcome
-        if self.throws.index(you)%3 > self.throws.index(opponent)%3: # win
+        if throw_you == (throw_opp+1)%3: # Win if your index is 1 higher than your opponent's mod 3, as R<P<S<R
             round_score += self.points['win']
-        elif self.throws.index(you)%3 == self.throws.index(opponent)%3: # draw
+        elif throw_you == throw_opp: # draw
             round_score += self.points['draw']
 
         self.total_score += round_score
